4f9ebd799e61621551f43cb52159a6d286886c10
John Eigenbrode <jeigenbrode@windstream.net>
2024-08-26 18:11:02 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by John Eigenbrode on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
