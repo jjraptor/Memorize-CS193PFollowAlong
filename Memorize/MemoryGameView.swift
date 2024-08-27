5fb4badd1237be48c1e4f7269659e61d3cefe0cd
John Eigenbrode <jeigenbrode@windstream.net>
2024-08-27 15:03:55 -0400

    Initial commit

--- Memorize/MemoryGameView.swift
+++ Memorize/MemoryGameView.swift
@@ -7,18 +7,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MemoryGameView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.orange)
+            Text("Hello, CS193P!")
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    MemoryGameView()
 }
