8b4d071c96d83791d5a53ce80a337af24137827d
John Eigenbrode <jeigenbrode@windstream.net>
2024-08-27 15:12:26 -0400

    L1

--- Memorize/MemoryGameView.swift
+++ Memorize/MemoryGameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MemoryGameView.swift
 //  Memorize
 //
 //  Created by John Eigenbrode on 8/26/24.
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct MemoryGameView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.orange)
-            Text("Hello, CS193P!")
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundStyle(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack(content:{
+            if isFaceUp{
+                RoundedRectangle(cornerRadius:12).fill(Color.white)
+                RoundedRectangle(cornerRadius:12).strokeBorder(lineWidth:2)
+                Text("👻").font(.largeTitle)
+            } else {
+                  // gets back color from above
+            }
+            
+        })
     }
 }
 
